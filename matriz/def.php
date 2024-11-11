ab971edd96a8ab4efb1806f4bb8c2399d6bdf401
Marvin <escorpiano18@bol.com.br>
2024-11-11 09:17:44 -0300

    k

---
+++ matriz/def.php
@@ -0,0 +1,19 @@
+<?php
+function soma($n1, $n2) {
+    $resultado = $n1 + $n2;
+    echo"Soma = $resultado";
+}
+function subtracao($n1, $n2) {
+    $resultado = $n1 - $n2;
+    echo"Subtração = $resultado";
+}
+function multiplicacao($n1, $n2) {
+    $resultado = $n1 * $n2;
+    echo"Multiplicação = $resultado";
+}
+function divisao($n1, $n2) {
+    $resultado = $n1 / $n2;
+    echo"Divisao= $resultado";
+}
+
+?>
--- matriz/switch.php
+++ matriz/switch.php
@@ -1,24 +1,22 @@
 <?php
+include("def.php");
+
 $n1 = $_POST["numero1"];
 $n2 = $_POST["numero2"];
 $operador = $_POST["operador"];
 
 switch ($operador){
     case "soma":
-        $resultado = $n1 + $n2;
-        echo"Soma = $resultado";
+        soma($n1, $n2);
         break;
     case "sub":
-        $resultado = $n1 - $n2;
-        echo "Subtração = $resultado";
+        subtracao($n1, $n2);
         break;
     case "mult":
-        $resultado = $n1 * $n2;
-        echo "Multiplicação = $resultado";
+        multiplicacao($n1, $n2);
         break;
     case "div":
-        $resultado = $n1 / $n2;
-        echo "Divisão = $resultado";
+        divisao($n1, $n2);
         break;
     default:
         echo"Operação invalida";
