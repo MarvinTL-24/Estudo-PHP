802d4163a747b4edf15295aca85bb8311a639dc9
Marvin <escorpiano18@bol.com.br>
2024-11-18 13:49:12 -0300

    ok

--- Banco de dados/ALunos/exibir.php
+++ Banco de dados/ALunos/exibir.php
@@ -2,40 +2,47 @@
 include_once("Conectar.php");
 include_once("head.html.php");
 
-// Substitua este valor pelo ID que você deseja buscar
+// Obtém o ID passado pela URL ou outro método
 $id = $_GET['id'] ?? null;
 
 if ($id) {
-    // Prepare a consulta SQL
+    // Prepara a consulta SQL usando o ID recebido
     $sql = "SELECT * FROM alunos WHERE Id = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $id);
-    $stmt->execute();
-    $resultado = $stmt->get_result();
 
-    if ($resultado && $linha = $resultado->fetch_assoc()) {
-        $imagem = htmlspecialchars($linha['imagem']);
-        $nome = htmlspecialchars($linha['nome']);
-        $cpf = htmlspecialchars($linha['cpf']);
-        $dataNascimento = htmlspecialchars($linha['dataNascimento']);
-        
-        echo "
-        <div class='row'>
-            <div class='card col-xl-3 m-3 p-3'>
-                <img src='$imagem' class='card-img-top' alt='Imagem do aluno'>
-                <div class='card-body'>
-                    <h5 class='card-title'>$nome</h5>
-                    <p class='card-text'>
-                        <i class='far fa-address-card'></i>
-                        <i>CPF: $cpf</i><br>
-                        <i class='far fa-address-card'></i>
-                        <i>Nascimento: $dataNascimento</i><br>
-                    </p>
+    if ($stmt) {
+        $stmt->bind_param("i", $id); // Liga o parâmetro ID à consulta
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+
+        if ($resultado && $linha = $resultado->fetch_assoc()) {
+            // Sanitiza os dados obtidos
+            $imagem = htmlspecialchars($linha['imagem']);
+            $nome = htmlspecialchars($linha['nome']);
+            $cpf = htmlspecialchars($linha['cpf']);
+            $dataNascimento = htmlspecialchars($linha['dataNascimento']);
+            
+            // Exibe as informações do aluno
+            echo "
+            <div class='row'>
+                <div class='card col-xl-3 m-3 p-3'>
+                    <img src='$imagem' class='card-img-top' alt='Imagem do aluno'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>$nome</h5>
+                        <p class='card-text'>
+                            <i class='far fa-address-card'></i>
+                            <i>CPF: $cpf</i><br>
+                            <i class='far fa-address-card'></i>
+                            <i>Nascimento: $dataNascimento</i><br>
+                        </p>
+                    </div>
                 </div>
-            </div>
-        </div>";
+            </div>";
+        } else {
+            echo "<p>Aluno não encontrado.</p>";
+        }
     } else {
-        echo "<p>Aluno não encontrado.</p>";
+        echo "<p>Erro ao preparar a consulta SQL.</p>";
     }
 } else {
     echo "<p>ID inválido ou não fornecido.</p>";
